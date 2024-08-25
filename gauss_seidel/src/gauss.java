d893272d4ae9c1ffc259d0e4fe8ae4d98c143a67
Gabriel.M <gamotasantos2000@gmail.com>
2024-08-25 00:50:39 -0400

    melhora na lógica

--- gauss_seidel/src/gauss.java
+++ gauss_seidel/src/gauss.java
@@ -1,23 +1,29 @@
-
+import java.util.Scanner;
 
 public class gauss {
     public static void main(String[] args) {
         
-        int n;
-        n = 3;
-        System.out.println(n);
-        int matriz[][];
-        matriz = new int [n][n];
+        
+        float matriz[][];
+        
+        System.out.println("Insira a ordem da matriz: ");
+        int n;n = 3;
+        //n = Scanner.System.in(nextInt());
+        
+        n +=1;
+        matriz = new float [n][n];
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 matriz[i][j] = i+j;
+                System.out.printf("Insira o termo a%d%d : %n",i+1,j+1);
             }
         }
 
         System.out.printf("%n");
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.printf("%d - ",matriz[i][j]);
+                System.out.printf("%.2f - ",matriz[i][j]);
             }
             System.out.printf("%n");
         }
